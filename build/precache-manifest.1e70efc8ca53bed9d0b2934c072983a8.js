8ef85f60137310348f1426341b4dc7980b5f95eb
Liam Rethore <lbr2016@gmail.com>
2020-09-01 16:04:18 -0700

    Deploy Site
    after npm run deploy

---
+++ build/precache-manifest.1e70efc8ca53bed9d0b2934c072983a8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36fb57e10672f07ad2ec168542160e2c",
+    "url": "/blr-portfolio/index.html"
+  },
+  {
+    "revision": "3673aed9016b086b1c29",
+    "url": "/blr-portfolio/static/css/main.6fafd636.chunk.css"
+  },
+  {
+    "revision": "1c6b7182e4223318186f",
+    "url": "/blr-portfolio/static/js/2.ad145645.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/blr-portfolio/static/js/2.ad145645.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3673aed9016b086b1c29",
+    "url": "/blr-portfolio/static/js/main.3657ba08.chunk.js"
+  },
+  {
+    "revision": "8c62d127d5e707b7b65c",
+    "url": "/blr-portfolio/static/js/runtime-main.dded8d63.js"
+  },
+  {
+    "revision": "50bb53e36d85c6cc85bf3894f9d9ac55",
+    "url": "/blr-portfolio/static/media/B Liam Rethore Resume.50bb53e3.pdf"
+  },
+  {
+    "revision": "42e16b34e7939191e27776e5790d11f5",
+    "url": "/blr-portfolio/static/media/Icon_New Array.42e16b34.png"
+  },
+  {
+    "revision": "a6ffc5d72a96b65159e710ea6d258ba4",
+    "url": "/blr-portfolio/static/media/Menlo Bold.a6ffc5d7.ttf"
+  },
+  {
+    "revision": "9f94dc20bb2a09c15241d3a880b7ad01",
+    "url": "/blr-portfolio/static/media/Menlo-Regular.9f94dc20.ttf"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.759cebf813b76aa5cb4d1ab242001834.js"
+  "/blr-portfolio/precache-manifest.1e70efc8ca53bed9d0b2934c072983a8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blr-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
