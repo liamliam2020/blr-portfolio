a6407b535dbe63334b32fb83e1c836ae82bf1b33
Liam Rethore <lbr2016@gmail.com>
2020-09-01 17:19:54 -0700

    Routing Issue
    Changed the homepage url to try and have routing work for both www and non

--- src/components/Navigation/Footer/Footer.js
+++ src/components/Navigation/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = (props) => (
   <footer className={styles.Footer} id="info">
     <section className={styles.FootMain}>
       <h2>
-        Reach me by email at
+        Reach me by email at{' '}
         <a href="mailto:bliamrethore@gmail.com">bliamrethore@gmail.com</a> or
         use one of the links below.
       </h2>
