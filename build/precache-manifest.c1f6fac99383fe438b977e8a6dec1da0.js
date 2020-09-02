d392fd4d04f3e25235fce27ee0e4f586b35d4296
Liam Rethore <lbr2016@gmail.com>
2020-09-01 20:27:11 -0700

    404 Workaround
    added a 404 redirect to try and hack around the react router not being supported

---
+++ build/precache-manifest.c1f6fac99383fe438b977e8a6dec1da0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1036b7b372e9e144aa0c1a291afed7f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78d47749f455e9ca6dbb",
+    "url": "/static/css/main.aec5069d.chunk.css"
+  },
+  {
+    "revision": "dc3b267946449a599dfb",
+    "url": "/static/js/2.05d9e39e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.05d9e39e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78d47749f455e9ca6dbb",
+    "url": "/static/js/main.1f1e291f.chunk.js"
+  },
+  {
+    "revision": "8af59409370a418eb6d2",
+    "url": "/static/js/runtime-main.74746cdb.js"
+  },
+  {
+    "revision": "50bb53e36d85c6cc85bf3894f9d9ac55",
+    "url": "/static/media/B Liam Rethore Resume.50bb53e3.pdf"
+  },
+  {
+    "revision": "42e16b34e7939191e27776e5790d11f5",
+    "url": "/static/media/Icon_New Array.42e16b34.png"
+  },
+  {
+    "revision": "a6ffc5d72a96b65159e710ea6d258ba4",
+    "url": "/static/media/Menlo Bold.a6ffc5d7.ttf"
+  },
+  {
+    "revision": "9f94dc20bb2a09c15241d3a880b7ad01",
+    "url": "/static/media/Menlo-Regular.9f94dc20.ttf"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 
 import './App.css';
 import Layout from './hoc/Layout/Layout';
 
 const App = (props) => {
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Layout />
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
